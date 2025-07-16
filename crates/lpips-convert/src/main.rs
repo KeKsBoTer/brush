97174d819c7c717fcb2f40fcc608a5c5cc3f05ee
Arthur Brussee <arthur.brussee@gmail.com>
2025-07-16 19:35:04 +0100

    Make lpips model functional (not added as a loss yet) (#209)
    * LPIPs v1
    
    * Move to convert project
    
    * Conver
    
    * Deny
    
    * Sum inline

---
+++ crates/lpips-convert/src/main.rs
@@ -0,0 +1,31 @@
+#![recursion_limit = "256"]
+
+use burn::module::Module;
+use burn::record::FullPrecisionSettings;
+use burn::record::HalfPrecisionSettings;
+use burn::record::Recorder;
+use burn::tensor::backend::Backend;
+use lpips::{LpipsModel, LpipsModelConfig};
+
+fn convert_lpips<B: Backend>(device: &B::Device) {
+    let model = LpipsModelConfig::new().init::<B>(device);
+    let record: <LpipsModel<B> as Module<B>>::Record =
+        burn_import::pytorch::PyTorchFileRecorder::<FullPrecisionSettings>::default()
+            .load(
+                burn_import::pytorch::LoadArgs::new("./lpips_vgg_remapped.pth".into()),
+                device,
+            )
+            .expect("Should decode state successfully");
+    let model = model.load_record(record);
+    let recorder = burn::record::NamedMpkGzFileRecorder::<HalfPrecisionSettings>::new();
+    model
+        .save_file("./burn_mapped", &recorder)
+        .expect("Failed to convert model");
+}
+
+fn main() {
+    println!("Converting LPIPS PyTorch model to Burn format...");
+    convert_lpips::<burn::backend::Wgpu>(&burn::backend::wgpu::WgpuDevice::default());
+    println!("Conversion completed successfully!");
+    println!("Output file: ./burn_mapped.mpk.gz");
+}
--- crates/lpips/src/lib.rs
+++ crates/lpips/src/lib.rs
@@ -1,64 +1,20 @@
 #![recursion_limit = "256"]
 
-use burn::nn::Initializer;
 use burn::nn::PaddingConfig2d;
-use burn::nn::Relu;
 use burn::nn::conv::Conv2d;
 use burn::nn::conv::Conv2dConfig;
 use burn::nn::pool::MaxPool2d;
 use burn::nn::pool::MaxPool2dConfig;
+use burn::record::HalfPrecisionSettings;
+use burn::record::NamedMpkGzFileRecorder;
+use burn::record::Recorder;
 use burn::tensor::Device;
+use burn::tensor::activation::relu;
 use burn::{
     config::Config,
     module::Module,
     tensor::{Tensor, backend::Backend},
 };
-// use burn_import::pytorch::{LoadArgs, PyTorchFileRecorder};
-use std::f64::consts::SQRT_2;
-
-struct ConvReluConfig {
-    conv: Conv2dConfig,
-}
-
-impl ConvReluConfig {
-    /// Create a new instance of the residual block [config](BasicBlockConfig).
-    fn new(in_channels: usize, out_channels: usize, stride: usize) -> Self {
-        // conv3x3
-        let conv = Conv2dConfig::new([in_channels, out_channels], [3, 3])
-            .with_stride([stride, stride])
-            .with_padding(PaddingConfig2d::Explicit(1, 1))
-            .with_bias(false);
-        Self { conv }
-    }
-
-    /// Initialize a new [basic residual block](BasicBlock) module.
-    fn init<B: Backend>(&self, device: &Device<B>) -> ConvRelu<B> {
-        // Conv initializer.
-        // TODO: Make sure this is lazy.
-        let initializer = Initializer::KaimingNormal {
-            gain: SQRT_2,
-            fan_out_only: true,
-        };
-
-        ConvRelu {
-            conv: self.conv.clone().with_initializer(initializer).init(device),
-            relu: Relu::new(),
-        }
-    }
-}
-
-#[derive(Module, Debug)]
-pub struct ConvRelu<B: Backend> {
-    conv: Conv2d<B>,
-    relu: Relu,
-}
-
-impl<B: Backend> ConvRelu<B> {
-    fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
-        let out = self.conv.forward(input);
-        self.relu.forward(out)
-    }
-}
 
 /// [Residual layer block](LayerBlock) configuration.
 #[derive(Config)]
@@ -73,16 +29,18 @@ impl VggBlockConfig {
     fn init<B: Backend>(&self, device: &Device<B>) -> VggBlock<B> {
         let convs = (0..self.num_blocks)
             .map(|b| {
-                ConvReluConfig::new(
-                    if b == 0 {
-                        self.in_channels
-                    } else {
-                        self.out_channels
-                    },
-                    self.out_channels,
-                    1,
-                )
-                .init(device)
+                let in_channels = if b == 0 {
+                    self.in_channels
+                } else {
+                    self.out_channels
+                };
+
+                // conv3x3
+                let conv = Conv2dConfig::new([in_channels, self.out_channels], [3, 3])
+                    .with_stride([1, 1])
+                    .with_padding(PaddingConfig2d::Explicit(1, 1))
+                    .with_bias(true);
+                conv.init(device)
             })
             .collect();
 
@@ -93,14 +51,14 @@ impl VggBlockConfig {
 #[derive(Module, Debug)]
 struct VggBlock<B: Backend> {
     /// A bottleneck residual block.
-    convs: Vec<ConvRelu<B>>,
+    convs: Vec<Conv2d<B>>,
 }
 
 impl<B: Backend> VggBlock<B> {
     pub(crate) fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
         let mut cur = input;
         for conv in &self.convs {
-            cur = conv.forward(cur);
+            cur = relu(conv.forward(cur));
         }
         cur
     }
@@ -109,25 +67,38 @@ impl<B: Backend> VggBlock<B> {
 #[derive(Module, Debug)]
 pub struct LpipsModel<B: Backend> {
     blocks: Vec<VggBlock<B>>,
+    heads: Vec<Conv2d<B>>,
     max_pool: MaxPool2d,
 }
 
 impl<B: Backend> LpipsModel<B> {
     pub fn forward(&self, patches: Tensor<B, 4>) -> Vec<Tensor<B, 4>> {
-        let mut fold = patches;
+        let shift = Tensor::<B, 1>::from_floats([-0.030, -0.088, -0.188], &patches.device())
+            .reshape([1, 3, 1, 1]);
+        let scale = Tensor::<B, 1>::from_floats([0.458, 0.448, 0.450], &patches.device())
+            .reshape([1, 3, 1, 1]);
+
+        let mut fold = (patches - shift) / scale;
+
         let mut res = vec![];
-        for block in &self.blocks {
+        for (i, block) in self.blocks.iter().enumerate() {
+            if i != 0 {
+                fold = self.max_pool.forward(fold);
+            }
+
             fold = block.forward(fold);
-            // Save intermediate state _before_ max pool as per the original lpips.
-            res.push(fold.clone());
-            fold = self.max_pool.forward(fold);
+
+            // Save intermediate state as normalized vec per lpips.
+            let norm_factor = fold.clone().powi_scalar(2).sum_dim(1).sqrt();
+            let normed = fold.clone() / (norm_factor + 1e-10);
+            res.push(normed);
         }
         res
     }
 
     /// Calculate the lpips. Imgs are in NCHW order. Inputs should be 0-1 normalised.
     pub fn lpips(&self, imgs_a: Tensor<B, 4>, imgs_b: Tensor<B, 4>) -> Tensor<B, 1> {
-        // Convert NHWC to NCHW
+        // Convert NHWC to NCHW and to [-1, 1].
         let imgs_a = imgs_a.permute([0, 3, 1, 2]);
         let imgs_b = imgs_b.permute([0, 3, 1, 2]);
 
@@ -135,23 +106,17 @@ impl<B: Backend> LpipsModel<B> {
         let imgs_a = self.forward(imgs_a * 2.0 - 1.0);
         let imgs_b = self.forward(imgs_b * 2.0 - 1.0);
 
-        // Get mean of spatial values.
-        let sim_values = imgs_a
-            .into_iter()
-            .zip(imgs_b)
-            // TODO: 3rd dimension is learned 1x1 convs....
-            .map(|(p1, p2)| {
-                (p1 - p2)
-                    .powf_scalar(2.0)
-                    .mean_dim(1)
-                    .mean_dim(2)
-                    .mean_dim(3)
-            })
-            .collect();
+        let device = imgs_a[0].device();
 
-        // Sum differences.
-        let sim_values = Tensor::cat(sim_values, 0);
-        sim_values.sum()
+        imgs_a.into_iter().zip(imgs_b).zip(&self.heads).fold(
+            Tensor::zeros([1], &device),
+            |acc, ((p1, p2), head)| {
+                let diff = (p1 - p2).powi_scalar(2);
+                let class = head.forward(diff);
+                // Add spatial mean.
+                acc + class.mean_dim(2).mean_dim(3).reshape([1])
+            },
+        )
     }
 }
 
@@ -161,103 +126,83 @@ pub struct LpipsModelConfig {}
 impl LpipsModelConfig {
     pub fn init<B: Backend>(&self, device: &B::Device) -> LpipsModel<B> {
         // Could have different variations here but just doing VGG for now.
-        let block1 = VggBlockConfig::new(2, 3, 64).init(device);
-        let block2 = VggBlockConfig::new(2, 64, 128).init(device);
-        let block3 = VggBlockConfig::new(3, 128, 256).init(device);
-        let block4 = VggBlockConfig::new(3, 256, 512).init(device);
-        let block5 = VggBlockConfig::new(3, 512, 512).init(device);
+        let blocks = [
+            (2, 3, 64),
+            (2, 64, 128),
+            (3, 128, 256),
+            (3, 256, 512),
+            (3, 512, 512),
+        ]
+        .iter()
+        .map(|&(num_blocks, in_channels, out_channels)| {
+            VggBlockConfig::new(num_blocks, in_channels, out_channels).init(device)
+        })
+        .collect();
+
+        let heads = [64, 128, 256, 512, 512]
+            .iter()
+            .map(|&channels| {
+                Conv2dConfig::new([channels, 1], [1, 1])
+                    .with_stride([1, 1])
+                    .with_bias(false)
+                    .init(device)
+            })
+            .collect();
 
         LpipsModel {
-            blocks: vec![block1, block2, block3, block4, block5],
+            blocks,
+            heads,
             max_pool: MaxPool2dConfig::new([2, 2]).with_strides([2, 2]).init(),
         }
     }
 }
 
-// pub fn load_vgg_lpips<B: Backend>(device: &B::Device) -> LpipsModel<B> {
-//     let model = LpipsModelConfig::new().init::<B>(device);
-//     let device = B::Device::default();
+pub fn load_vgg_lpips<B: Backend>(device: &B::Device) -> LpipsModel<B> {
+    let model = LpipsModelConfig::new().init::<B>(device);
+    model.load_record(
+        NamedMpkGzFileRecorder::<HalfPrecisionSettings>::default()
+            .load("./burn_mapped".into(), device)
+            .expect("Should decode state successfully"),
+    )
+}
 
-//     let mut load_args = LoadArgs::new("./vgg16_conv_float32.pth".into());
+#[cfg(test)]
+mod tests {
+    use super::load_vgg_lpips;
+    use burn::backend::Wgpu;
+    use burn::backend::wgpu::WgpuDevice;
+    use burn::tensor::TensorData;
+    use burn::tensor::{Tensor, backend::Backend};
+    use image::ImageReader;
 
-//     let key_mappings = [
-//         // PyTorch Idx | Burn Block Idx | Burn Conv Idx in Block
-//         ("0", 0, 0),
-//         ("2", 0, 1),
-//         ("5", 1, 0),
-//         ("7", 1, 1),
-//         ("10", 2, 0),
-//         ("12", 2, 1),
-//         ("14", 2, 2),
-//         ("17", 3, 0),
-//         ("19", 3, 1),
-//         ("21", 3, 2),
-//         ("24", 4, 0),
-//         ("26", 4, 1),
-//         ("28", 4, 2),
-//     ];
+    fn image_to_tensor<B: Backend>(device: &B::Device, img: &image::DynamicImage) -> Tensor<B, 4> {
+        // Convert to RGB float array
+        let rgb_img = img.to_rgb32f();
+        let (w, h) = rgb_img.dimensions();
+        let data = TensorData::new(rgb_img.into_vec(), [1, h as usize, w as usize, 3]);
+        Tensor::from_data(data, device)
+    }
 
-//     for (pt_idx_str, burn_block_idx, burn_conv_idx) in &key_mappings {
-//         load_args = load_args.with_key_remap(
-//             &format!(r"^{pt_idx_str}\.(weight|bias)$"),
-//             &format!(r"blocks.{burn_block_idx}.convs.{burn_conv_idx}.conv.$1"),
-//         );
-//     }
+    #[test]
+    fn test_result() -> Result<(), Box<dyn std::error::Error>> {
+        let device = WgpuDevice::default();
 
-//     // TODO: Load linear logs.
+        // Load and preprocess the images
+        let image1 = ImageReader::open("./apple.png")?.decode()?;
+        let image2 = ImageReader::open("./pear.png")?.decode()?;
 
-//     let record: <LpipsModel<B> as Module<B>>::Record =
-//         PyTorchFileRecorder::<FullPrecisionSettings>::default()
-//             .load(load_args, &device)
-//             .expect("Should decode state successfully");
+        let apple = image_to_tensor::<Wgpu>(&device, &image1);
+        let pear = image_to_tensor::<Wgpu>(&device, &image2);
 
-//     println!("record {:?}", record.blocks.len());
-//     model.load_record(record)
-// }
+        let model = load_vgg_lpips(&device);
 
-// #[cfg(test)]
-// mod tests {
-//     use super::load_vgg_lpips;
-//     use burn::backend::Wgpu;
-//     use burn::backend::wgpu::WgpuDevice;
-//     use burn::tensor::TensorData;
-//     use burn::tensor::{Tensor, backend::Backend};
-//     use image::ImageReader;
+        // Calculate LPIPS similarity score between the two images
+        let similarity_score = model.lpips(apple, pear).into_scalar();
 
-//     fn image_to_tensor<B: Backend>(device: &B::Device, img: &image::DynamicImage) -> Tensor<B, 4> {
-//         // Convert to RGB float array
-//         let rgb_img = img.to_rgb32f().into_vec();
-//         let data = TensorData::new(rgb_img, [1, 64, 64, 3]);
-//         Tensor::from_data(data, device)
-//     }
+        println!("LPIPS similarity score: {similarity_score}");
 
-//     #[test]
-//     fn test_result() -> Result<(), Box<dyn std::error::Error>> {
-//         let device = WgpuDevice::default();
+        assert!((similarity_score - 0.65710217).abs() < 1e-4);
 
-//         // Load and preprocess the images
-//         let image1 = ImageReader::open("./apple.jpg")?.decode()?;
-//         let image2 = ImageReader::open("./pear.png")?.decode()?;
-
-//         let tensor1 = image_to_tensor::<Wgpu>(&device, &image1);
-//         let tensor2 = image_to_tensor::<Wgpu>(&device, &image2);
-
-//         println!(
-//             "Converted images to tensors with shape: {:?} and {:?}",
-//             tensor1.shape(),
-//             tensor2.shape()
-//         );
-
-//         // Load the LPIPS model
-//         let model = load_vgg_lpips(&device);
-
-//         // Calculate LPIPS similarity score between the two images
-//         let similarity_score = model.lpips(tensor1, tensor2).into_scalar();
-
-//         println!("LPIPS similarity score: {similarity_score}");
-
-//         assert_eq!(similarity_score, 0.6731404);
-
-//         Ok(())
-//     }
-// }
+        Ok(())
+    }
+}
